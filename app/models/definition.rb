3e50282b93f9b3563523cf332b83a73361f2d369
Shawn Peng <shawn-peng@outlook.com>
2019-08-08 18:13:56 +0800

    Update definition.rb
    no need to do this as it belongs to user

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -2,7 +2,6 @@ class Definition < ApplicationRecord
   belongs_to :slang
   belongs_to :user
   has_many :likes
-  validates :user, presence: true
   # validates :slang, uniqueness: true
   validates :slang, uniqueness: { scope: :user }
 end
