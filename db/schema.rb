22736bdde08c8208def6e4a94dcbd1bebfc470b1
PShawnKD35 <shawn-peng@outlook.com>
2019-08-12 21:46:58 +0800

    Changed sticker url to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_075103) do
+ActiveRecord::Schema.define(version: 2019_08_12_125427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_08_12_075103) do
   create_table "slangs", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id"
-    t.string "sticker_url"
+    t.text "sticker_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_slangs_on_user_id"
