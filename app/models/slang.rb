b29e7579acd18220775d72a287c0a1306ca03007
Shawn <shawn-peng@outlook.com>
2019-08-13 18:13:45 +0800

    Added tags validation

--- app/models/slang.rb
+++ app/models/slang.rb
@@ -6,6 +6,7 @@ class Slang < ApplicationRecord
   acts_as_favoritable
 
   validates :name, presence: true, uniqueness: true, allow_blank: false
+  validate :validate_tag
 
   before_save :update_pinyin, if: :name_changed?
 
@@ -13,5 +14,14 @@ class Slang < ApplicationRecord
 
   def update_pinyin
     self.pinyin = PinYin.of_string(name, :unicode).join(' ')
+  end
+
+  def validate_tag
+    max_tags = 20
+    errors[:tag_list] << "#{max_tags} tags maximum" if tag_list.count > max_tags
+    max_tag_length = 15
+    tag_list.each do |tag|
+      errors.add(:tag_list, "Tag exceeded maximum length of #{max_tag_length}: #{tag}") unless tag.length <= max_tag_length
+    end
   end
 end
